be24b4155fcb03944dd79bd4ef1c626c11f61cec
chan-hyeok-kim <daiolous@gmail.com>
2024-02-13 21:05:21 +0900

    BFS, maze algorithm

---
+++ Algorithm/Player.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm
+{
+    class Player
+    {
+        public int PosX { get; private set; }
+        public int PosY { get; private set; }
+        Random _random = new Random();   
+        Map _map;
+        public void Initialize(int posY, int posX, int destY, int dextX, Map map)
+        {
+            PosY = posY;
+            PosX = posX;
+            _map = map;
+        }
+
+        const int MOVE_TICK = 100;
+        int _sumTick = 0;
+        
+        public void Update(int deltaTick)
+        {
+            _sumTick += deltaTick;
+            if(_sumTick >= MOVE_TICK) { 
+               _sumTick = 0;
+
+                // 여기에다가 0.1초마다 실행될 로직을 넣어준다
+                int randValue = _random.Next(0, 5);
+                switch(randValue)
+                {
+                    case 0:  // 상
+                        _map.Tile
+                    case 1:  // 하
+                    case 2:  // 좌
+                    case 3:  // 우
+
+                }
+            }
+        }
+    }
+}
--- Algorithm/Program.cs
+++ Algorithm/Program.cs
@@ -11,33 +11,33 @@
             board.InitializeLinked();
             Console.CursorVisible = false;
 
-          
-            const char CIRCLE = '\u25cf';
-           
+            Map map = new Map();
+            Player player = new Player();
+            map.Initialize(25, player);
+            player.Initialize(1, 1, map.Size-2, map.Size-2, player);
 
-
+            const int WAIT_TICK = 1000 / 30;
+            int lastTick = 0;
             while (true)
             {
-               
+                int currentTick = Environment.TickCount & Int32.MaxValue;
+                if (currentTick - lastTick < WAIT_TICK)
+                    continue;
+                int deltaTick = currentTick - lastTick;
+                lastTick = currentTick; 
                 //입력
                 //로직
+                player.Update(deltaTick); 
+
                 //렌더링
 
-               Console.SetCursorPosition(0, 0);
-                for(int i = 0; i < 25; i++)
-                {
-                    for (int j = 0; j < 25; j++)
-                    {
-                        Console.ForegroundColor = ConsoleColor.Blue; 
-                        Console.Write(CIRCLE);
-                    }
-                    Console.WriteLine();
-                }
+                Console.SetCursorPosition(0, 0);
+                map.Render();
 
-                #region 프레임 관리
+              /*  #region 프레임 관리
                 Thread.Sleep(1000 / 30); // 30fps로 제한, 초당 30번 렌더링
 
-                #endregion
+                #endregion*/
             }
         }
     }
